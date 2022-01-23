cd000ec149fa45dba4a520ae21aa13091384d384
Scott Laptop <burdick.10@wright.edu>
2022-01-23 13:26:03 -0500

    burdick - added the base water globe.  properly sized and placed.  have not tied the two objects together.

--- OBJImporter/OBJImporter/OBJImporter.cxx
+++ OBJImporter/OBJImporter/OBJImporter.cxx
@@ -5,6 +5,7 @@
 #include <vtkPolyDataMapper.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+
 #include <vtkRenderer.h>
 #include <vtkTexture.h>
 #include <vtkSphereSource.h>
@@ -12,6 +13,99 @@
 #include <vtkCylinderSource.h>
 #include <vtkProperty.h>
 #include <vtkCamera.h>
+
+#include <vtkInteractorStyleTrackballCamera.h>
+
+int size = 50;
+
+//brought to global scope to allow keyboard callback to update
+vtkNew<vtkOBJImporter> importer;
+vtkNew<vtkRenderWindow> renWin;
+vtkNew<vtkSphereSource> sphere;
+
+
+namespace {
+
+    // Define interaction style
+    class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
+    {
+    public:
+        static KeyPressInteractorStyle* New();
+        vtkTypeMacro(KeyPressInteractorStyle, vtkInteractorStyleTrackballCamera);
+
+        virtual void OnKeyPress() override
+        {
+            // Get the keypress
+            vtkRenderWindowInteractor* rwi = this->Interactor;
+            std::string key = rwi->GetKeySym();
+            
+            
+            
+            //importer->Update();
+            
+            // Output the key that was pressed
+            std::cout << "Pressed " << key << std::endl;
+            double datal[3];
+            sphere->GetCenter(datal);
+            std::cout << "Sphere: [ " << datal[0] << ", " << datal[1] << ", " << datal[2] << ", " << sphere->GetRadius() << "]" << std::endl;
+            // Handle an arrow key
+            if (key == "Up")
+            {
+                sphere->SetCenter(datal[0] + .1, datal[1], datal[2]);
+                std::cout << "The up arrow was pressed." << std::endl;
+            }
+            if (key == "Down")
+            {
+                sphere->SetCenter(datal[0] - .1, datal[1], datal[2]);
+                std::cout << "The dn arrow was pressed." << std::endl;
+            }
+            if (key == "Left")
+            {
+                sphere->SetCenter(datal[0], datal[1]+.1, datal[2]);
+                std::cout << "The lt arrow was pressed." << std::endl;
+            }
+            if (key == "Right")
+            {
+                sphere->SetCenter(datal[0], datal[1] - .1, datal[2]);
+                std::cout << "The rt arrow was pressed." << std::endl;
+            }
+            if (key == "Prior")
+            {
+                sphere->SetRadius(sphere->GetRadius()+1);
+                std::cout << "The PgUp was pressed." << std::endl;
+            }
+            if (key == "Next")
+            {
+                if (sphere->GetRadius() >= 1) {
+                    sphere->SetRadius(sphere->GetRadius() - 1);
+                }
+                std::cout << "The Pg Down was pressed." << std::endl;
+            }
+            if (key == "o")
+            {
+                sphere->SetCenter(datal[0], datal[1], datal[2]+.1);
+                std::cout << "The PgUp was pressed." << std::endl;
+            }
+            if (key == "l")
+            {
+                
+                sphere->SetCenter(datal[0], datal[1], datal[2]-.1);
+                std::cout << "The Pg Down was pressed." << std::endl;
+            }
+            // Handle a "normal" key
+            if (key == "a")
+            {
+                std::cout << "The a key was pressed." << std::endl;
+            }
+            renWin->Render();
+            // Forward events
+            vtkInteractorStyleTrackballCamera::OnKeyPress();
+        }
+    };
+    vtkStandardNewMacro(KeyPressInteractorStyle);
+
+} // namespace
+
 
 int main(int argc, char* argv[])
 {
@@ -24,7 +118,7 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
-  vtkNew<vtkOBJImporter> importer;
+  
   importer->SetFileName(argv[1]);
   importer->SetFileNameMTL(argv[2]);
   importer->SetTexturePath(argv[3]);
@@ -33,118 +127,49 @@ int main(int argc, char* argv[])
 
   vtkNew<vtkRenderer> renderer;
  
-  vtkNew<vtkRenderWindow> renWin;
+  
   vtkNew<vtkRenderWindowInteractor> iren;
 
   renderer->SetBackground2(colors->GetColor3d("Silver").GetData());
-  renderer->SetBackground(colors->GetColor3d("Gold").GetData());
+  renderer->SetBackground(.2,.2,.4);
   
   renderer->GradientBackgroundOn();
-  renWin->AddRenderer(renderer);
-  renderer->UseHiddenLineRemovalOn();
-  renWin->AddRenderer(renderer);
-  renWin->SetSize(640, 480);
-  renWin->SetWindowName("OBJImporter");
-
-  iren->SetRenderWindow(renWin);
-  importer->SetRenderWindow(renWin);
-  importer->Update();
-
-
-
-//add a base sphere that is tied to the size of the globe?
-  
+    
   //read the points?
   vtkSmartPointer<vtkPolyData> polyData;
-  //auto polyData = ReadPolyData(argv[i]);
-  //vtkNew<vtkSphereSource> source;
-  //source->Update();
-  //polyData = source->GetOutput();
+  
 
-  vtkNew<vtkNamedColors> colors2;
-
-  // Set the background color.
-  std::array<unsigned char, 4> bkg{ {26, 51, 102, 255} };
-  colors2->SetColor("BkgColor", bkg.data());
-
-  // This creates a polygonal cylinder model with eight circumferential facets
-  // (i.e, in practice an octagonal prism).
-  vtkNew<vtkCylinderSource> cylinder;
-  cylinder->SetResolution(8);
-
-  // The mapper is responsible for pushing the geometry into the graphics
-  // library. It may also do color mapping, if scalars or other attributes are
-  // defined.
-  vtkNew<vtkPolyDataMapper> cylinderMapper;
-  cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
-
-  // The actor is a grouping mechanism: besides the geometry (mapper), it
-  // also has a property, transformation matrix, and/or texture map.
-  // Here we set its color and rotate it around the X and Y axes.
-  vtkNew<vtkActor> cylinderActor;
-  cylinderActor->SetMapper(cylinderMapper);
-  cylinderActor->GetProperty()->SetColor(
-      colors2->GetColor4d("Tomato").GetData());
-  cylinderActor->RotateX(30.0);
-  cylinderActor->RotateY(-45.0);
-
-  // The renderer generates the image
-  // which is then displayed on the render window.
-  // It can be thought of as a scene to which the actor is added
-  vtkNew<vtkRenderer> renderer2;
-  renderer2->AddActor(cylinderActor);
-  renderer2->SetBackground(colors->GetColor3d("BkgColor").GetData());
-  // Zoom in a little by accessing the camera and invoking its "Zoom" method.
-  renderer2->ResetCamera();
-  renderer2->GetActiveCamera()->Zoom(0.5);
-
-  // The render window is the actual GUI window
-  // that appears on the computer screen
-  //vtkNew<vtkRenderWindow> renderWindow;
-  //renderWindow->SetSize(300, 300);
-  //renderWindow->AddRenderer(renderer2);
-  //enderWindow->SetWindowName("Cylinder");
-
-  vtkNew<vtkActor> cylinderActor2;
-  cylinderActor2->SetMapper(cylinderMapper);
-  cylinderActor2->GetProperty()->SetColor(
-      colors2->GetColor4d("Green").GetData());
-  cylinderActor2->RotateX(30.0);
-  cylinderActor2->RotateY(-75.0);
-  vtkNew<vtkRenderer> renderer3;
-  renderer3->AddActor(cylinderActor2);
-  renderer3->SetBackground(colors->GetColor3d("BkgColor").GetData());
-  // Zoom in a little by accessing the camera and invoking its "Zoom" method.
-  renderer3->ResetCamera();
-  renderer3->GetActiveCamera()->Zoom(0.25);
-
-  vtkNew<vtkSphereSource> sphere;
+  //model the ocean under the wireframe
+  sphere->SetRadius(58.4);
+  sphere->SetPhiResolution(100);
+  sphere->SetThetaResolution(100);
+  sphere->SetLatLongTessellation(true);
   vtkNew<vtkPolyDataMapper> sphereMapper;
   sphereMapper->SetInputConnection(sphere->GetOutputPort());
   vtkNew<vtkActor> sphereActor;
   sphereActor->SetMapper(sphereMapper);
-  sphereActor->SetOrigin(2, 1, 3);
-  sphereActor->RotateY(6);
-  sphereActor->SetPosition(2.25, 0, 0);
-  sphereActor->GetProperty()->SetColor(1, 0, 0);
-  renderer2->AddActor(sphereActor);
+  sphereActor->SetOrigin(0, 0, 0);
+  sphereActor->SetPosition(13.2, 7.4, -21.1);
+  sphereActor->GetProperty()->SetColor(0, 0, 1);
+  renderer->AddActor(sphereActor);
+  //end sphere
 
+  //add the parts to the window for display
+  renWin->AddRenderer(renderer);
+  renderer->UseHiddenLineRemovalOn();
+  renWin->AddRenderer(renderer);
+  renWin->SetSize(800, 600);
+  renWin->SetWindowName("Homework 1 - points on a globe");
+  //2 5.5, -20.6, 57
+  iren->SetRenderWindow(renWin);
+  vtkNew<KeyPressInteractorStyle> style;
+  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+  renderWindowInteractor->SetRenderWindow(renWin);
+  renderWindowInteractor->SetInteractorStyle(style);
+  style->SetCurrentRenderer(renderer);
 
-
-  renWin->AddRenderer(renderer2);
-  //renWin->AddRenderer(renderer3);
-
-  // The render window interactor captures mouse events
-  // and will perform appropriate camera or actor manipulation
-  // depending on the nature of the events.
-  //vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
-  //renderWindowInteractor->SetRenderWindow(renderWindow);
-
-  // This starts the event loop and as a side effect causes an initial render.
-  //renderWindow->Render();
-  //renderWindowInteractor->Start();
-
-
+  importer->SetRenderWindow(renWin);
+  importer->Update();
 
   auto actors = renderer->GetActors();
   actors->InitTraversal();
@@ -164,12 +189,12 @@ int main(int argc, char* argv[])
       actor->GetTexture()->InterpolateOn();
     }
 
-    vtkPolyData* pd =
-        dynamic_cast<vtkPolyData*>(actor->GetMapper()->GetInput());
+    //vtkPolyData* pd =
+      //  dynamic_cast<vtkPolyData*>(actor->GetMapper()->GetInput());
 
-    vtkPolyDataMapper* mapper =
-        dynamic_cast<vtkPolyDataMapper*>(actor->GetMapper());
-    mapper->SetInputData(pd);
+//    vtkPolyDataMapper* mapper =
+  //      dynamic_cast<vtkPolyDataMapper*>(actor->GetMapper());
+    //mapper->SetInputData(pd);
   }
   renWin->Render();
   iren->Start();
